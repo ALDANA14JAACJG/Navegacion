c57fe6542d6de36458c1d9cb4f678b4c94997165
Jorge Aldana <jorge.acarrillo@alumnos.udg.mx>
2022-05-25 00:09:18 -0500

    Initial commit

---
+++ app/src/main/java/com/aldana/jorge/navegacion/SegundoFragmento.kt
@@ -0,0 +1,28 @@
+package com.aldana.jorge.navegacion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class SegundoFragmento : Fragment() {
+
+    lateinit var boton: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val vista = inflater.inflate(R.layout.fragment_segundo_fragmento, container, false)
+        boton = vista.findViewById(R.id.button2)
+        boton.setOnClickListener {
+            findNavController().navigate(R.id.action_segundoFragmento_to_primerFragmento)
+        }
+        return vista
+    }
+
+}
